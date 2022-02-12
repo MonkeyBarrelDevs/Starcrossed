b619912cea843191a1c528ecc5a0d053931ae799
TinyDeath2 <owlman02@gmail.com>
2022-02-12 14:22:54 -0700

    LevelLoader script
    added level loader script from previous jam (not hooked up)

---
+++ Feb2022GameJam/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] Animator transition;
+    [SerializeField] float screenHoldTime = 2.5f;
+    [SerializeField] float transitionTime;
+
+    private void Start()
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 0)
+            StartCoroutine(Delay(screenHoldTime));
+    }
+
+    public void LoadNextLevel()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    public void ReplayTransition()
+    {
+        StartCoroutine(CycleTransition());
+    }
+
+    public void LoadLevelAtIndex(int index)
+    {
+        StartCoroutine(LoadLevel(index));
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(levelIndex);
+    }
+
+    IEnumerator CycleTransition()
+    {
+        yield return new WaitForSeconds(transitionTime);
+
+    }
+
+    IEnumerator Delay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        LoadNextLevel();
+    }
+}
