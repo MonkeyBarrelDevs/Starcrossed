05983cb28c56b8ea7c0f6927a8815d01cba8463a
TinyDeath2 <owlman02@gmail.com>
2022-02-12 12:30:53 -0700

    Black hole #2
    Added the second black hole and different control setups for each

--- Feb2022GameJam/Assets/Scripts/PlayerController.cs
+++ Feb2022GameJam/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D playerRidge;
+    public int playerNumber;
 
     void Start()
     {
@@ -13,7 +14,16 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-        Vector3 playerInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        Vector2 playerInput = new Vector2(0, 0);
+        if (playerNumber == 1)
+        {
+            playerInput = new Vector2(Input.GetAxis("Horizontal 1"), Input.GetAxis("Vertical 1"));
+        }
+        else if (playerNumber == 2)
+        {
+            playerInput = new Vector2(Input.GetAxis("Horizontal 2"), Input.GetAxis("Vertical 2"));
+        }
         playerRidge.velocity = playerInput * 7;
     }
 }
